bfb43255cd1e68775daae4f71396ca74141f9355
tarcantcode <wiletgg@gmail.com>
2026-04-01 02:37:56 +0700

    Add Roman to Integer conversion class
    Implement a class to convert Roman numerals to integers with a helper function for value mapping.

---
+++ 13. Roman to Integer/roman-to-interger.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int value(char c) { 
+        if (c == 'I') return 1;
+        if (c == 'V') return 5;
+        if (c == 'X') return 10;
+        if (c == 'L') return 50;
+        if (c == 'C') return 100;
+        if (c == 'D') return 500;
+        if (c == 'M') return 1000;
+        return 0;
+    }
+
+    int romanToInt(string s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 < s.length()){
+                if (value(s[i]) < value(s[i + 1])) {
+                    res += value(s[i + 1]) - value(s[i]);
+                    i++;
+                } else {
+                    res += value(s[i]);
+                }
+            } else {
+                res += value(s[i]);
+            }
+    }
+    return res;
+  }
+};
